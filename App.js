55761f5c42300de69590e34298632c7abd874c15
Felipe10142 <felipe.albornoz@estudante.sesisenai.org.br>
2025-09-03 10:01:35 -0300

    docs: Atualizando README.md com gepeto

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>oI, MUNDO! VAI SE FODER!!!!!!!!!</Text>
+      <Text>oI, MUNDO! !!!!!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
