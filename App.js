3489ce7100805502538132fde63072fc80e1234f
Felipe10142 <felipe.albornoz@estudante.sesisenai.org.br>
2025-09-03 11:01:48 -0300

    feat: Ultima atualização do readme calaio

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
-import LoginScreen from './screens/LoginScreen';
-import JSON from './screens/JSON';
-import TicketScreen from './screens/TicketScreen';
+import { View, Image, Text, StyleSheet } from 'react-native';
 
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo ao Intervalinho </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: '1px',
+    alignItems: 'center',
+    backgroundColor: '#000000ff',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 20,
+    color: '#rgba(255, 255, 255, 1)',
+  },
+});
