b2b136577e577c0fbade9ea496d072319e5c097e
Felipe10142 <felipe.albornoz@estudante.sesisenai.org.br>
2025-09-03 10:37:55 -0300

    feat: Atualizando README.md (de novo) e criando a arquitectura do projeto pra sofrer

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import JSON from './screens/JSON';
+import TicketScreen from './screens/TicketScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>oI, MUNDO! !!!!!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
